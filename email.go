adf98031b4a52bb0315f6a9e4da64d5e871e2e8e
CL.Lam <lamciuloeng@gmail.com>
2014-05-09 15:11:17 +0800

    update the email.go

--- email.go
+++ email.go
@@ -6,15 +6,92 @@ import (
     "strings"
     "fmt"
     "encoding/base64"
+    "path"
+    "crypto/rand"
+    "io/ioutil"
+    "io"
 )
 
+func randomBoundary() string {
+    var buf [30]byte
+    _, err := io.ReadFull(rand.Reader, buf[:])
+    if err != nil {
+	panic(err)
+    }
+    return fmt.Sprintf("%x", buf[:])
+}
+
+func encodeStr(str string)string{
+    return base64.StdEncoding.EncodeToString([]byte(str))
+}
+
+func encodeTitle(str string)string {
+    // "=?UTF-8?X?  X -> B for Base64, Q for Quoted-Printable 
+    return fmt.Sprintf("=?UTF-8?b?%s?=",encodeStr(str))
+}
+
+func createTextMsg(from string,to string,cc string,subject string,content string)string {
+    hdr := make(map[string]string)
+    hdr["From"] = from 
+    hdr["To"] = to
+    hdr["Cc"] = cc
+    hdr["Subject"] = encodeTitle(subject)
+    hdr["MIME-Version"] = "1.0"
+    hdr["Content-Type"] = "text/html; charset=UTF-8"
+    hdr["Content-Transfer-Encoding"] = "base64"
+
+    msg := ""
+    for k,v := range hdr {
+	msg += fmt.Sprintf("%s: %s\r\n",k,v)
+    }
+    msg += fmt.Sprintf("\r\n%s" ,encodeStr(content)) 
+    return msg 
+}
+
+
+func createMultipartMsg(from string,to string,cc string,subject string,body string,files []string) (string,error) {
+    msg := ""
+    boundary := randomBoundary() 
+    msg += "MIME-Version: 1.0"
+    msg += fmt.Sprintf("\r\nFrom: %s",from)
+    msg += fmt.Sprintf("\r\nTo: %s",to)
+    msg += fmt.Sprintf("\r\nCc: %s",cc)
+    msg += fmt.Sprintf("\r\nSubject: %s",encodeTitle(subject))
+    msg += fmt.Sprintf("\r\nContent-Type: multipart/mixed; boundary=%s",boundary)
+
+    //add the email body
+    msg += fmt.Sprintf("\r\n\r\n--%s",boundary)
+    msg += "\r\nContent-Type: text/html; charset=UTF-8"
+    msg += "\r\nContent-Transfer-Encoding: base64"
+    msg += fmt.Sprintf("\r\n\r\n%s",encodeStr(body))
+
+    //add the attachment
+    for _,f := range files{
+	filename := path.Base(f)
+	content,err := ioutil.ReadFile(f)
+	if err != nil { return "",err }
+	encodedContent := encodeStr(string(content))
+	msg += fmt.Sprintf("\r\n--%s",boundary)
+        msg += "\r\nContent-Type: application/octet-stream"
+	msg += "\r\nMIME-Version: 1.0"
+	msg += "\r\nContent-Transfer-Encoding: base64"
+	msg += fmt.Sprintf("\r\nContent-Disposition: attachment; filename=\"%s\"",encodeTitle(filename))
+	msg += fmt.Sprintf("\r\n\r\n%s",encodedContent)
+    }
+
+    msg += fmt.Sprintf("\r\n--%s--",boundary)
+    return msg,nil
+
+}
+
 func SendEmail(from string,
-                 to []string,
-		 cc []string,
-		 subject string,
-		 content string,
-		 serverip string,
-		 port string) error{
+		to []string,
+		cc []string,
+		subject string,
+		body string,
+		files []string,
+		serverip string,
+		port string) error{
 
     emailfrom := mail.Address{from,from}
     var sendto,emailto,emailcc []string;
@@ -29,23 +106,26 @@ func SendEmail(from string,
 	emailcc = append(emailcc,tmp.String())
 	sendto = append(sendto,tmp.Address)
     }
-
-    hdr := make(map[string]string)
-    hdr["From"] = emailfrom.String()
-    hdr["To"] = strings.Join(emailto,",")
-    hdr["Cc"] = strings.Join(emailcc,",")
-    // "=?UTF-8?X?  X -> B for Base64, Q for Quoted-Printable 
-    hdr["Subject"] = fmt.Sprintf("=?UTF-8?B?%s?=",base64.StdEncoding.EncodeToString([]byte(subject)))
-    hdr["MIME-Version"] = "1.0"
-    hdr["Content-Type"] = "text/html; charset=UTF-8"
-    hdr["Content-Transfer-Encoding"] = "base64"
-
-    msg := ""
-    for k,v := range hdr {
-	msg += fmt.Sprintf("%s: %s\r\n",k,v)
+    
+    var msg string
+    var err error  
+    if len(files) < 1 {
+	msg = createTextMsg(emailfrom.String(), 
+	                     strings.Join(emailto,","),
+			     strings.Join(emailcc,","),
+			     subject,
+			     body)
+    }else{
+        msg ,err = createMultipartMsg(emailfrom.String(), 
+	                     strings.Join(emailto,","),
+			     strings.Join(emailcc,","),
+			     subject,
+			     body,
+			     files)
+	if err != nil { return err }
     }
-    msg += "\r\n" + base64.StdEncoding.EncodeToString([]byte(content)) 
+    
     srv := serverip + ":" + port
-    err := smtp.SendMail(srv,nil,emailfrom.Address,sendto,[]byte(msg))
-    return err
+    emailerr := smtp.SendMail(srv,nil,emailfrom.Address,sendto,[]byte(msg))
+    return emailerr
 }
